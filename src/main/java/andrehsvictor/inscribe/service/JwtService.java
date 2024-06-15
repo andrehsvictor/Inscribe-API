379bb2849bb6ead03c1ea5480db6bf4e8380a28c
André Victor <andrehsvictor@gmail.com>
2024-06-15 20:41:30 -0300

    feat: Add JwtService for generating and refreshing JWT tokens
    This commit adds the `JwtService` class, which is responsible for generating and refreshing JWT tokens. It includes methods for generating tokens based on user authentication and refreshing tokens based on a provided refresh token. This service enhances the user authentication functionality by providing secure token generation and refreshing capabilities.

---
+++ src/main/java/andrehsvictor/inscribe/service/JwtService.java
@@ -0,0 +1,49 @@
+package andrehsvictor.inscribe.service;
+
+import java.time.Instant;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.stereotype.Service;
+
+import andrehsvictor.inscribe.payload.response.TokenResponse;
+
+@Service
+public class JwtService {
+
+    @Autowired
+    private JwtEncoder jwtEncoder;
+
+    @Autowired
+    private RefreshTokenService refreshTokenService;
+
+    private Long expirationMillis = 900_000L;
+
+    public TokenResponse generate(Authentication authentication) {
+        Instant expiration = Instant.now().plusMillis(expirationMillis);
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .subject(authentication.getName())
+                .expiresAt(expiration)
+                .issuedAt(Instant.now())
+                .build();
+
+        String accessToken = jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+        String refreshToken = refreshTokenService.generate(authentication);
+
+        return TokenResponse.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .createdAt(Date.from(claims.getIssuedAt()).toString())
+                .expiresAt(Date.from(claims.getExpiresAt()).toString())
+                .build();
+    }
+
+    public TokenResponse refresh(String refreshToken) {
+        Authentication authentication = refreshTokenService.retrieveAuthenticationByRefreshToken(refreshToken);
+        return generate(authentication);
+    }
+}
