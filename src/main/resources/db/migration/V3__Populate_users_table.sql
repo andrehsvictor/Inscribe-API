e8abcc83cb27632667aefe186d5164d59bc4ddc8
André Victor <andrehsvictor@gmail.com>
2024-06-15 15:57:51 -0300

    feat: Add SQL migration script to populate users table
    This commit adds a new SQL migration script `V3__Populate_users_table.sql` that inserts two users into the `users` table. The script includes the public ID, name, email, password, and active status for each user. This migration script is necessary to populate the initial data for the users table in the application.

---
+++ src/main/resources/db/migration/V3__Populate_users_table.sql
@@ -0,0 +1,19 @@
+INSERT INTO
+    users (public_id, name, email, password, active)
+VALUES
+    (
+        'fcdff7984fb2445fb9ce03d034323dda',
+        'André Victor',
+        'andrevictor@inscribe.io',
+        '$2a$12$0RrQo147KNSauCFikKNzvObGgm3Qy1x0jzP9ZVJqc8nzR4nhCEBF2',
+        -- password: 12345678
+        TRUE
+    ),
+    (
+        '28dce48427694a5396cb1587759f200a',
+        'João Marcos',
+        'joaomarcos@inscribe.io',
+        '$2a$12$0RrQo147KNSauCFikKNzvObGgm3Qy1x0jzP9ZVJqc8nzR4nhCEBF2',
+        -- password: 12345678
+        TRUE
+    );
