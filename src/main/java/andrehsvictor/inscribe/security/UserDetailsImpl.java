97f15ca29d09035e7e2081d5762013e1dd449356
André Victor <andrehsvictor@gmail.com>
2024-06-15 20:41:15 -0300

    feat: Add UserDetailsImpl class for handling user details in security
    This commit adds the `UserDetailsImpl` class, which implements the `UserDetails` interface. It includes methods for retrieving user authorities, password, username, and enabling status. The class is used for handling user details in the security module of the application.

---
+++ src/main/java/andrehsvictor/inscribe/security/UserDetailsImpl.java
@@ -0,0 +1,36 @@
+package andrehsvictor.inscribe.security;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import andrehsvictor.inscribe.entity.User;
+import lombok.Data;
+
+@Data
+public class UserDetailsImpl implements UserDetails {
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getEmail();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return user.isActive() && !user.isDeleted();
+    }
+}
