c6e6ee784b30519fd726ea90eaf0bf7ca3c614f6
André Victor <andrehsvictor@gmail.com>
2024-06-16 16:37:00 -0300

    feat: Add existsByEmail method to UserRepository
    The UserRepository interface has been updated to include a new method, existsByEmail, which checks if a user with the given email exists in the database. This addition enhances the functionality of the application by providing a convenient way to check for the existence of a user based on their email address.

--- src/main/java/andrehsvictor/inscribe/repository/UserRepository.java
+++ src/main/java/andrehsvictor/inscribe/repository/UserRepository.java
@@ -11,5 +11,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query("SELECT u FROM User u WHERE u.email = :email AND u.deleted = false")
     Optional<User> findByEmail(String email);
-    
+
+    @Query("SELECT CASE WHEN COUNT(u) > 0 THEN true ELSE false END FROM User u WHERE u.email = :email")
+    boolean existsByEmail(String email);
+
 }
