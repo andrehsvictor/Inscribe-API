fa4950633a2b7d49fd63d0fbdcf52fde74d07790
André Victor <andrehsvictor@gmail.com>
2024-06-16 11:17:19 -0300

    feat: Add validation for email and password fields in SigninRequest
    This commit adds validation for the email and password fields in the SigninRequest class. The email field is required and must be between 5 and 100 characters long. The password field is also required and must be between 8 and 100 characters long. This addition ensures that the input data for signing in is properly validated before processing.

--- src/main/java/andrehsvictor/inscribe/payload/request/SigninRequest.java
+++ src/main/java/andrehsvictor/inscribe/payload/request/SigninRequest.java
@@ -1,9 +1,13 @@
 package andrehsvictor.inscribe.payload.request;
 
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class SigninRequest {
+    @Size(min = 5, max = 100, message = "Email must be between 5 and 100 characters")
     private String email;
+
+    @Size(min = 8, max = 100, message = "Password must be between 8 and 100 characters")
     private String password;
 }
