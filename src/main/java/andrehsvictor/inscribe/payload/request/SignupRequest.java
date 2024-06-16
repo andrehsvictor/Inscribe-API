08a24d4384982dc2d5ab511393aac24820c1ec8e
André Victor <andrehsvictor@gmail.com>
2024-06-16 16:36:49 -0300

    feat: Add validation annotations to SignupRequest fields
    The `SignupRequest` class in the `payload.request` package has been updated to include validation annotations for the `name`, `email`, and `password` fields. This addition enhances the data integrity of the application by enforcing constraints on the input values for these fields.

--- src/main/java/andrehsvictor/inscribe/payload/request/SignupRequest.java
+++ src/main/java/andrehsvictor/inscribe/payload/request/SignupRequest.java
@@ -1,12 +1,23 @@
 package andrehsvictor.inscribe.payload.request;
 
 import andrehsvictor.inscribe.entity.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+import lombok.ToString;
 
 @Data
+@ToString(exclude = "password")
 public class SignupRequest {
+    
+    @Size(min = 3, max = 20, message = "Name must be between 3 and 20 characters")
     private String name;
+
+    @Size(min = 6, max = 50, message = "Password must be between 6 and 50 characters")
+    @Email(message = "Invalid email")
     private String email;
+
+    @Size(min = 6, max = 50, message = "Password must be between 6 and 50 characters")
     private String password;
 
     public User toUser() {
