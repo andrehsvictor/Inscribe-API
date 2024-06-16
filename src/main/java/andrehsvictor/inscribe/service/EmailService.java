89cdb1235169d47d8338f49bdf61e44a1f03bb46
André Victor <andrehsvictor@gmail.com>
2024-06-16 16:37:22 -0300

    feat: Update EmailService to use MailSender instead of JavaMailSender
    The EmailService class in the service package has been updated to use the MailSender interface instead of the JavaMailSender implementation. This change enhances the flexibility of the application by allowing for easier integration with different email sending providers.

--- src/main/java/andrehsvictor/inscribe/service/EmailService.java
+++ src/main/java/andrehsvictor/inscribe/service/EmailService.java
@@ -1,8 +1,8 @@
 package andrehsvictor.inscribe.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMailMessage;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
@@ -14,19 +14,18 @@ import jakarta.mail.internet.MimeMessage;
 public class EmailService {
 
     @Autowired
-    private JavaMailSender javaMailSender;
+    private MailSender mailSender;
 
     public void send(String to, String subject, String text) {
-        MimeMailMessage message = new MimeMailMessage(javaMailSender.createMimeMessage());
-        MimeMessage mimeMessage = message.getMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage);
         try {
+            MimeMessage message = ((JavaMailSender) mailSender).createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
             helper.setTo(to);
             helper.setSubject(subject);
             helper.setText(text, true);
+            ((JavaMailSender) mailSender).send(message);
         } catch (MessagingException e) {
             throw new InscribeException("Failed to send email", 500);
         }
-        javaMailSender.send(mimeMessage);
     }
 }
