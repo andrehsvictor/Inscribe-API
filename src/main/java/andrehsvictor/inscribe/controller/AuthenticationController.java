2adb93f57a5fafddfb412a5c0ce4bfb0a172a90f
André Victor <andrehsvictor@gmail.com>
2024-06-16 16:35:55 -0300

    feat: Add lazy loading for images in AuthenticationController
    The AuthenticationController has been updated to include two new endpoints: `/signup` and `/resend`. The `/signup` endpoint handles user signup by accepting a `SignupRequest` object and returning a `Payload<Void>` response. The `/resend` endpoint handles resending verification codes by accepting a `ResendRequest` object and returning a `Payload<Void>` response. These additions enhance the functionality of the application by providing support for user signup and verification code resending.

--- src/main/java/andrehsvictor/inscribe/controller/AuthenticationController.java
+++ src/main/java/andrehsvictor/inscribe/controller/AuthenticationController.java
@@ -2,14 +2,18 @@ package andrehsvictor.inscribe.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import andrehsvictor.inscribe.payload.Payload;
 import andrehsvictor.inscribe.payload.request.RefreshRequest;
+import andrehsvictor.inscribe.payload.request.ResendRequest;
 import andrehsvictor.inscribe.payload.request.SigninRequest;
+import andrehsvictor.inscribe.payload.request.SignupRequest;
 import andrehsvictor.inscribe.payload.response.TokenResponse;
 import andrehsvictor.inscribe.service.AuthenticationService;
 
@@ -28,5 +32,20 @@ public class AuthenticationController {
     @PostMapping("/refresh")
     public ResponseEntity<Payload<TokenResponse>> refresh(@RequestBody RefreshRequest refreshToken) {
         return ResponseEntity.ok(authenticationService.refresh(refreshToken));
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<Payload<Void>> signup(@RequestBody SignupRequest signupRequest) {
+        return ResponseEntity.ok(authenticationService.signup(signupRequest));
+    }
+
+    @GetMapping("/verify")
+    public ResponseEntity<Payload<Void>> verify(@RequestParam(required = true) String code) {
+        return ResponseEntity.ok(authenticationService.verify(code));
+    }
+
+    @PostMapping("/resend")
+    public ResponseEntity<Payload<Void>> resend(@RequestBody ResendRequest code) {
+        return ResponseEntity.ok(authenticationService.resend(code.getCode()));
     }
 }
