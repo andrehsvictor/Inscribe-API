2e4c000927f2101368b6f8076a555e99f3af70e3
André Victor <andrehsvictor@gmail.com>
2024-06-16 11:19:40 -0300

    refactor: Remove 'slug' column from notes table and update V4__Populate_notes_table.sql
    This commit removes the unused 'slug' column from the notes table and updates the V4__Populate_notes_table.sql migration file accordingly. The 'slug' column was no longer needed for the application's functionality, so it was removed to simplify the database structure. This change improves the efficiency and maintainability of the application by eliminating unnecessary data storage and database operations.

--- src/main/resources/db/migration/V4__Populate_notes_table.sql
+++ src/main/resources/db/migration/V4__Populate_notes_table.sql
@@ -1,31 +1,27 @@
 INSERT INTO
-    notes (public_id, title, slug, content, user_id)
+    notes (public_id, title, content, user_id)
 VALUES
     (
         'c215061aa77a48829ff3486f83e8211f',
         'Lista de Compras',
-        'lista-de-compras',
         'Leite, Ovos, Pão, Manteiga, Queijo',
         1
     ),
     (
         'ad9e76cf47de419890fb07bc80962c1c',
         'Reunião com Cliente',
-        'reuniao-com-cliente',
         'Discutir os requisitos do projeto e próximos passos.',
         1
     ),
     (
         '554c589711d9464692ed539c9dc6ab9d',
         'Exercícios Matinais',
-        'exercicios-matinais',
         '30 minutos de corrida, 15 minutos de alongamento.',
         2
     ),
     (
         '8069f9e87e70492e8af67857c8bcc522',
         'Planejamento de Viagem',
-        'planejamento-de-viagem',
         'Pesquisar hotéis e voos para o Rio de Janeiro.',
         2
     );
