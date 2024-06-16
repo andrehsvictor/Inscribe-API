911a931fd4823eac5bf58b18323016e8d91bd46b
André Victor <andrehsvictor@gmail.com>
2024-06-16 16:37:31 -0300

    feat: Add email validation during user signup

--- src/main/java/andrehsvictor/inscribe/service/SignupService.java
+++ src/main/java/andrehsvictor/inscribe/service/SignupService.java
@@ -1,22 +1,33 @@
 package andrehsvictor.inscribe.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import andrehsvictor.inscribe.entity.User;
+import andrehsvictor.inscribe.exception.InscribeException;
 import andrehsvictor.inscribe.payload.request.SignupRequest;
 import andrehsvictor.inscribe.repository.UserRepository;
 
+@Service
 public class SignupService {
-    
+
     @Autowired
     private VerificationService verificationService;
 
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public void signup(SignupRequest signupRequest) {
         User user = signupRequest.toUser();
+        if (userRepository.existsByEmail(user.getEmail())) {
+            throw new InscribeException("Email already in use", 400);
+        }
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         user = userRepository.save(user);
-        verificationService.sendVerificationCode(user);
+        verificationService.sendVerificationEmail(user);
     }
 }
