65c130a72df5ec2ba8aec128258b9ef83ffae67f
André Victor <andrehsvictor@gmail.com>
2024-06-18 08:34:35 -0300

    refactor: Add @AllArgsConstructor annotation to SigninRequest class

--- src/main/java/andrehsvictor/inscribe/payload/request/SigninRequest.java
+++ src/main/java/andrehsvictor/inscribe/payload/request/SigninRequest.java
@@ -1,9 +1,11 @@
 package andrehsvictor.inscribe.payload.request;
 
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class SigninRequest {
     @Size(min = 5, max = 100, message = "Email must be between 5 and 100 characters")
     private String email;
